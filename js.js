afe8bb04bd2404fc440b429724c7e2742f77d2b4
Gaboper79 <gp130579@gmail.com>
2022-05-18 11:20:59 -0300

    primer commit

---
+++ js.js
@@ -0,0 +1,14 @@
+/*
+Fizzbuzz
+imprima desde 1 a 100
+ multipo  3 fizz
+ mulplo 5 buzz
+ ambos fizzbuss 
+  
+ */
+
+/* for (i = 1; i <= 100; i++) {
+  let rta = `${i % 3 ? "" : "fizz"}${i % 5 ? "" : "Buzz"}`;
+
+  console.log(rta || i);
+} */
